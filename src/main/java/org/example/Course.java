a2831dd5cfaf2dcd487ea0d45d799cb33a8624ab
Erika Trixie P. Dirilo <erikadirilo23@gmail.com>
2026-02-07 09:01:26 +0800

    yeah

---
+++ src/main/java/org/example/Course.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class Course {
+    private int courseID;
+    private String courseName;
+    private String program;
+
+    public Course(){
+
+    }
+
+    public Course( int courseID, String courseName, String program){
+        this.courseID = courseID;
+        this.courseName = courseName;
+        this.program = program;
+    }
+
+    public int getCourseID(){
+        return courseID;
+    }
+
+    public void setCourseID(int courseID){
+        this.courseID = courseID;
+    }
+
+    public String getCourseName(){
+        return courseName;
+    }
+
+    public void setCourseName(String courseName){
+        this.courseName = courseName;
+    }
+
+    public String getProgram(){
+        return program;
+    }
+
+    public void setProgram(String program){
+        this.program = program;
+    }
+}
