98dd8bb9bcbf43ddab717716f1dfe7aefbd5dd70
Erika Trixie P. Dirilo <erikadirilo23@gmail.com>
2026-03-07 08:07:15 +0800

    Third Commit: Added Calculator and Calculator Test to service package

---
+++ src/main/java/org/example/service/Calculator.java
@@ -0,0 +1,16 @@
+package org.example.service;
+
+public class Calculator {
+    //add
+    //substract
+    //division
+    //multiplication
+
+    public int sum(int a, int b){
+        return a + b;
+    }
+
+    public int subtract(int a, int b){
+        return a - b;
+    }
+}
