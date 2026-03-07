1b145067d652a20d72e34546a53fb4020903e0e2
Erika Trixie P. Dirilo <erikadirilo23@gmail.com>
2026-03-07 11:34:38 +0800

    Removed imports

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,9 @@
 package org.example;
-import org.example.model.Person;
 import org.example.model.Student;
 import org.example.model.Instructor;
-import org.example.model.Course;
+
 
 public class Main{
 
         }
-}
+
