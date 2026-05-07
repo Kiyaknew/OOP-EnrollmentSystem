d820ac11a679f63f447a9fe6b2809fdc7e7e62ab
Erika Trixie P. Dirilo <erikadirilo23@gmail.com>
2026-05-08 03:35:19 +0800

    Added model - Tuition Fee Payment

---
+++ src/main/java/org/example/model/TuitionFeePayment.java
@@ -0,0 +1,12 @@
+package org.example.model;
+
+public class TuitionFeePayment {
+    private double pricePerUnit = 1000.00;
+    private double balance;
+    private double totalTuition;
+    private Student student; //uhhhh i think
+
+    public TuitionFeePayment(){
+
+    }
+}
