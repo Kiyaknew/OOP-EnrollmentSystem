5b30d902558e25429213f2a1b851e5431f4dbb40
Erika Trixie P. Dirilo <erikadirilo23@gmail.com>
2026-05-08 02:59:34 +0800

    Updated entity - Section

--- src/main/java/org/example/model/Section.java
+++ src/main/java/org/example/model/Section.java
@@ -6,9 +6,67 @@ import java.util.List;
 public class Section {
     private String sectionID;
     private String sectionName;
-    private List<Instructor> instructorList;
-    private List<Student> studentList;
+    private int maxCapacity;
+    private Course program;
+    private Instructor assignedInstructor;
+    private List<Student> enrolledList; //dunno if this is correct, complete other entities first :/
 
     public Section(){
+
     }
+
+    public Section(String sectionID){
+        this.sectionID = sectionID;
+    }
+
+    public Section(String sectionID, String sectionName, int maxCapacity, Course program){
+        this.sectionID = sectionID;
+        this.sectionName = sectionName;
+        this.maxCapacity = maxCapacity;
+        this.program = program;
+        this.enrolledList = new ArrayList<>();
+    }
+
+    public String getSectionID(){
+        return sectionID;
+    }
+
+    public void setSectionID(String sectionID){
+        this.sectionID = sectionID;
+    }
+
+    public String getSectionName(){
+        return sectionName;
+    }
+
+    public void setSectionName(String sectionName){
+        this.sectionName = sectionName;
+    }
+
+    public int getMaxCapacity(){
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity){
+        this.maxCapacity = maxCapacity;
+    }
+
+    public Course getProgram(){
+        return program;
+    }
+
+    public void setProgram(Course program){
+        this.program = program;
+    }
+
+    public Instructor getAssignedInstructor(){
+        return assignedInstructor;
+    }
+
+    public void setAssignedInstructor(Instructor assignedInstructor){
+        this.assignedInstructor = assignedInstructor;
+    }
+
+
 }
+
