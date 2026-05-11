1c2ce0f0e8eec90b6f802129d17fa4e2ad063242
Erika Trixie P. Dirilo <erikadirilo23@gmail.com>
2026-05-11 17:35:14 +0800

    updated main menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,48 +1,70 @@
 package org.example;
 
 import org.example.model.*;
-import org.example.service.CampusRegistrar;
-import org.example.service.CourseRegistrationImpl;
-import org.example.service.InstructorRegistrationImp;
-import org.example.service.StudentRegistrationImp;
+import org.example.service.*;
 
 import java.util.Scanner;
 
+//gonna try doing the main with static methods for better redirecting of menus cuz its alot
 public class Main {
     static Scanner input = new Scanner(System.in);
 
+    //all implementations
+    static StudentRegistrationImp studentReg = new StudentRegistrationImp();
+    static CourseRegistrationImpl courseReg = new CourseRegistrationImpl();
+    static InstructorRegistrationImp instructorReg = new InstructorRegistrationImp();
+    static SectionRegistrationImp sectionReg = new SectionRegistrationImp();
+    static DepartmentRegistrationImp depReg = new DepartmentRegistrationImp();
+    static EnrollmentRegistrationImp enrollReg = new EnrollmentRegistrationImp(studentReg, sectionReg, depReg);
+    static TuitionRegistrationImp tuitionReg = new TuitionRegistrationImp(studentReg);
+
+    //campus reg
+    static CampusRegistrar registrar = new CampusRegistrar(studentReg, courseReg,
+            instructorReg, sectionReg, depReg, enrollReg, tuitionReg);
+
+
     public static void main(String[] args) {
-        Student student = new Student();
-        student.mainTask();
-
-        Instructor instructor = new Instructor();
-        instructor.mainTask();
-
-        StudentRegistrationImp studentRegistration = new StudentRegistrationImp();
-        CourseRegistrationImpl courseRegistration = new CourseRegistrationImpl();
-
-        //interactive
-        while (true) {
-            System.out.print("\n---DLSL Enrollment and Management System---\n" +
-                    "1. Campus Management\n" +
-                    "2. Enroll Student\n"  +
-                    "3. Exit\n" +
-                    "Choice: ");
-
-            int choice1 = input.nextInt();
-
-            if (choice1 == 1) {
-                System.out.print("\n---Campus Management---\n" +
-                        "1. Instructor and Assigned Course Management\n" +
-                        "2. Student Management\n" +
-                        "3. Course Management\n" +
-                        "4. Section Management\n" +
-                        "5. Department Management\n" +
-                        "Enter choice: ");
-                int srNum = input.nextInt();
-                input.nextLine();
-
+        boolean runSystem = true;
+        while (runSystem) {
+            System.out.println("|-------------- Welcome to E.X.E Campus System -------------|");
+            System.out.println("= 1 = Student Management");
+            System.out.println("= 2 = Course Management");
+            System.out.println("= 3 = Instructor Management");
+            System.out.println("= 4 = Section Management");
+            System.out.println("= 5 = Department Management");
+            System.out.println("= 6 = Enroll Student");
+            System.out.println("= 7 = Calculate Student Tuition");
+            System.out.println("= 0 = Exit the System");
+            System.out.println("|-------------- ------------------------------ -------------|");
+            System.out.println(">>>| Select a number: ");
+            switch (input.nextLine()) {
+                case "1":
+                    studentMenu();
+                case "2":
+                    courseMenu();
+                case "3":
+                    instructorMenu();
+                case "4":
+                    sectionMenu();
+                case "5":
+                    departmentMenu();
+                case "6":
+                    enrollmentMenu();
+                case "7":
+                    tuitionMenu();
+                case "0":
+                    runSystem = false;
+                default:
+                    System.out.println("Invalid choice!");
             }
         }
+        System.out.println("Thank you for using E.X.E Campus System.");
     }
+    //student menu
+
+    static void studentMenu(){
+
+    }
+
+
 }
